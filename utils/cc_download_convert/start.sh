20ac403cd8bcffe6c79377d117b1b18721ea2794
Messede Degod <messede_degod@protonmail.com>
2025-05-24 14:04:18 +0000

    add: domain cleanup utility

--- utils/cc_download_convert/start.sh
+++ utils/cc_download_convert/start.sh
@@ -1,1 +1,16 @@
+# Download and extract domains
+if [ ! -f clean_domains ]; then
+    go build convert.go
+fi
 time cat cc-index.paths  | while read x; do bash ./download_and_convert.sh $x; done
+
+# Sort domains
+sort -u domains > a
+mv a domains
+
+# Clean domains
+if [ ! -f clean_domains ]; then
+    go build clean_domains.go
+fi
+cat domains | ./clean_domains > a
+mv a domains
